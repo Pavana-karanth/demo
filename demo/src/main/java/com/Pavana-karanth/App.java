76dff4edba7d5c33c1171dc8799da9acfa33a577
Pavana. P. Karanth <pavana.karanth17@gmail.com>
2025-03-14 11:12:31 +0530

    Update App.java

--- demo/src/main/java/com/Pavana-karanth/App.java
+++ demo/src/main/java/com/Pavana-karanth/App.java
@@ -2,12 +2,9 @@ package com.Pavana-karanth;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main(String[] args)
-    {
+public class App {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
